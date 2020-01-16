67b28f4d2fc019468b1ddaa39bd2fab5cedc995a
Liu Xue Yan <liu_xue_yan@foxmail.com>
2020-01-16 15:29:16 +0800

    fix: export resources outputs

--- src/mkdocs_nbconvert/plugin.py
+++ src/mkdocs_nbconvert/plugin.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import shutil
 import sys
 from glob import iglob
 from pprint import pformat
@@ -30,7 +31,6 @@ class NbConvertPlugin(BasePlugin):
 
     def __init__(self, *args, **kwargs):
         self._logger = logging.getLogger('mkdocs.plugins.NbConvertPlugin')
-        self._exported_paths = []
         super(NbConvertPlugin, self).__init__(*args, **kwargs)
 
     def on_files(self, files, config):
@@ -39,10 +39,10 @@ class NbConvertPlugin(BasePlugin):
         # deal with dirs
         config_file_dir = os.path.dirname(config['config_file_path'])
         input_dir = os.path.normpath(self.config['input_dir'])
-        output_dir = os.path.join(
+        output_dir = os.path.realpath(os.path.join(
             config['docs_dir'],
             os.path.normpath(self.config['output_dir'])
-        )
+        ))
         if not os.path.isabs(input_dir):
             input_dir = os.path.realpath(os.path.join(config_file_dir, input_dir))
         # glob match
@@ -59,30 +59,46 @@ class NbConvertPlugin(BasePlugin):
             nb_dirname, nb_basename = os.path.split(nb_path)
             nb_basename_root, _ = os.path.splitext(nb_basename)
             nb_subdir = os.path.relpath(nb_dirname, input_dir)
-            md_dir = os.path.realpath(os.path.join(output_dir, nb_subdir))
+            md_dir = os.path.join(output_dir, nb_subdir)
             md_basename = '{0}.md'.format(nb_basename_root)
             md_path = os.path.join(md_dir, md_basename)
             md_rel_dir = os.path.relpath(md_dir, config['docs_dir'])
             md_rel_path = os.path.join(md_rel_dir, md_basename)
             #
-            logger.info('nbconvert: markdown export %s => %s', nb_path, md_path)
+            logger.debug('nbconvert: markdown export %s => %s', nb_path, md_path)
             # run nbconvert
             with open(nb_path) as fp:
                 nb_node = nbformat.read(fp, nbformat.NO_CONVERT)
-            body, resources = md_exporter.from_notebook_node(nb_node)  # pylint:disable=unused-variable
+            body, resources = md_exporter.from_notebook_node(nb_node)
             # save exported
             if not os.path.exists(md_dir):
                 os.makedirs(md_dir)
             with open(md_path, 'w', encoding='UTF8') as fp:
                 fp.write(body)
-            self._exported_paths.append(md_path)
             file_obj = File(
                 path=md_rel_path,
                 src_dir=config['docs_dir'],
                 dest_dir=config['site_dir'],
                 use_directory_urls=config['use_directory_urls']
             )
-            logger.info(
+
+            for resource_name, resource_data in resources['outputs'].items():
+                resource_src_dir = os.path.dirname(file_obj.abs_src_path)
+                resource_src_path = os.path.join(resource_src_dir, resource_name)
+                if not os.path.isdir(resource_src_dir):
+                    os.makedirs(resource_src_dir)
+                with open(resource_src_path, 'wb') as fp:
+                    fp.write(resource_data)
+                resource_dest_dir = os.path.dirname(file_obj.abs_dest_path)
+                resource_dest_path = os.path.join(resource_dest_dir, resource_name)
+                logger.debug('nbconvert: resource output(%dBytes): resource_name --> %s',
+                             len(resource_data), resource_dest_path)
+                if not os.path.isdir(resource_dest_dir):
+                    os.makedirs(resource_dest_dir)
+                with open(resource_dest_path, 'wb') as fp:
+                    fp.write(resource_data)
+
+            logger.debug(
                 'nbconvert: add file object<abs_src_path=%s abs_dest_path=%s url=%s>',
                 file_obj.abs_src_path, file_obj.abs_dest_path, file_obj.url
             )
@@ -90,5 +106,8 @@ class NbConvertPlugin(BasePlugin):
         return files
 
     def on_post_build(self, config):  # pylint:disable=unused-argument
-        for path in self._exported_paths:
-            os.remove(path)
+        output_dir = os.path.join(
+            config['docs_dir'],
+            os.path.normpath(self.config['output_dir'])
+        )
+        shutil.rmtree(output_dir)
