a2261c1bf431be2722f0b0b8a7bdea68f4d92dd2
Liu Xue Yan <liu_xue_yan@foxmail.com>
2021-12-03 15:45:59 +0800

    optimize...

--- src/mkdocs_nbconvert/plugin.py
+++ src/mkdocs_nbconvert/plugin.py
@@ -1,7 +1,7 @@
 import io
-import os
 import sys
 from glob import iglob
+from os import makedirs, path, remove, removedirs
 from pprint import pformat
 
 import nbformat
@@ -33,36 +33,36 @@ class NbConvertPlugin(BasePlugin):
         log.info('nbconvert: plugin config=%s', pformat(self.config))
         self._src_files = []
         # deal with dirs
-        config_file_dir = os.path.dirname(config['config_file_path'])
-        input_dir = os.path.normpath(self.config['input_dir'])
-        output_dir = os.path.realpath(os.path.join(
+        config_file_dir = path.dirname(config['config_file_path'])
+        input_dir = path.normpath(self.config['input_dir'])
+        output_dir = path.realpath(path.join(
             config['docs_dir'],
-            os.path.normpath(self.config['output_dir'])
+            path.normpath(self.config['output_dir'])
         ))
-        if not os.path.isabs(input_dir):
-            input_dir = os.path.realpath(os.path.join(config_file_dir, input_dir))
+        if not path.isabs(input_dir):
+            input_dir = path.realpath(path.join(config_file_dir, input_dir))
         # glob match
         glob_recursive = self.config['recursive'] if PYTHON_VERSION_MAJOR_MINOR >= '3.5' else False
         if glob_recursive:
             nb_paths_iter = iglob(
-                os.path.join(config_file_dir, input_dir, '**', '*.ipynb'),
+                path.join(config_file_dir, input_dir, '**', '*.ipynb'),
                 recursive=True
             )
         else:
-            nb_paths_iter = iglob(os.path.join(config_file_dir, input_dir, '*.ipynb'))
+            nb_paths_iter = iglob(path.join(config_file_dir, input_dir, '*.ipynb'))
         # Exporter
         exporter = MarkdownExporter()
         # Converting
         for nb_path in nb_paths_iter:
             # Prepare output file/dir
-            nb_dirname, nb_basename = os.path.split(nb_path)
-            nb_basename_root, _ = os.path.splitext(nb_basename)
-            nb_subdir = os.path.relpath(nb_dirname, input_dir)
-            md_dir = os.path.join(output_dir, nb_subdir)
+            nb_dirname, nb_basename = path.split(nb_path)
+            nb_basename_root, _ = path.splitext(nb_basename)
+            nb_subdir = path.relpath(nb_dirname, input_dir)
+            md_dir = path.join(output_dir, nb_subdir)
             md_basename = '{0}.md'.format(nb_basename_root)
-            md_path = os.path.join(md_dir, md_basename)
-            md_rel_dir = os.path.relpath(md_dir, config['docs_dir'])
-            md_rel_path = os.path.join(md_rel_dir, md_basename)
+            md_path = path.join(md_dir, md_basename)
+            md_rel_dir = path.relpath(md_dir, config['docs_dir'])
+            md_rel_path = path.join(md_rel_dir, md_basename)
             #
             log.debug(
                 'nbconvert: markdown export %s => %s',
@@ -73,8 +73,8 @@ class NbConvertPlugin(BasePlugin):
                 nb_node = nbformat.read(fp, nbformat.NO_CONVERT)
             body, resources = exporter.from_notebook_node(nb_node)
             # save exported
-            if not os.path.exists(md_dir):
-                os.makedirs(md_dir)
+            if not path.exists(md_dir):
+                makedirs(md_dir)
             with io.open(md_path, 'w', encoding='utf-8') as fp:
                 fp.write(body)
             file_obj = File(
@@ -85,21 +85,21 @@ class NbConvertPlugin(BasePlugin):
             )
 
             for resource_name, resource_data in resources['outputs'].items():
-                resource_src_dir = os.path.dirname(file_obj.abs_src_path)
-                resource_src_path = os.path.join(resource_src_dir, resource_name)
-                if not os.path.isdir(resource_src_dir):
-                    os.makedirs(resource_src_dir)
+                resource_src_dir = path.dirname(file_obj.abs_src_path)
+                resource_src_path = path.join(resource_src_dir, resource_name)
+                if not path.isdir(resource_src_dir):
+                    makedirs(resource_src_dir)
                 with io.open(resource_src_path, 'wb') as fp:
                     fp.write(resource_data)
                 self._src_files.append(resource_src_path)
-                resource_dest_dir = os.path.dirname(file_obj.abs_dest_path)
-                resource_dest_path = os.path.join(resource_dest_dir, resource_name)
+                resource_dest_dir = path.dirname(file_obj.abs_dest_path)
+                resource_dest_path = path.join(resource_dest_dir, resource_name)
                 log.debug(
                     'nbconvert: resource output(%dBytes): %s => %s',
                     len(resource_data), resource_name, resource_dest_path
                 )
-                if not os.path.isdir(resource_dest_dir):
-                    os.makedirs(resource_dest_dir)
+                if not path.isdir(resource_dest_dir):
+                    makedirs(resource_dest_dir)
                 with io.open(resource_dest_path, 'wb') as fp:
                     fp.write(resource_data)
 
@@ -114,13 +114,13 @@ class NbConvertPlugin(BasePlugin):
     def on_post_build(self, config, **kwargs):
         for file in self._src_files:
             log.debug('nbconvert: remove %s', file)
-            os.remove(file)
-        output_dir = os.path.join(
+            remove(file)
+        output_dir = path.join(
             config['docs_dir'],
-            os.path.normpath(self.config['output_dir'])
+            path.normpath(self.config['output_dir'])
         )
         log.debug('nbconvert: removedirs %s', output_dir)
         try:
-            os.removedirs(output_dir)
+            removedirs(output_dir)
         except OSError as err:
             log.warning('nbconvert: removedirs %s', err)
