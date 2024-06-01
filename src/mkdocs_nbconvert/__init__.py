7546612a7ec03ae1e35f635225a03116b022306a
tanbro <tanbro@163.com>
2024-06-01 22:27:59 +0800

    ci: upgrade pre-commit

--- src/mkdocs_nbconvert/__init__.py
+++ src/mkdocs_nbconvert/__init__.py
@@ -1,2 +1,4 @@
-from ._version import __version__, __version_tuple__
-from .plugin import *
+__all__ = ["__version__", "__version_tuple__", "version", "version_tuple", "NbConvertPlugin"]
+
+from ._version import __version__, __version_tuple__, version, version_tuple
+from .plugin import NbConvertPlugin
